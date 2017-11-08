27ca05aa7fd257d53f5cf914195984a3b6c1e880
Conor Twomey <conor.twomey1@mycit.ie>
2017-11-08 10:45:48 +0000

    Create loop in js script

--- 2.setup_cluster.js
+++ 2.setup_cluster.js
@@ -8,7 +8,7 @@
 // 0. Set myName to the name of the computer
 //------------------------------------------------
 //
-var myName = "COM-IT22-51246";
+var myName = "DESKTOP-GIC9RBH";
 //
 //------------------------------------------------
 // 1. GLOBAL VARIABLES (I don't understand this)
@@ -30,182 +30,48 @@ db.settings.save( { _id:"chunksize", value: 1 } )
 //
 // 1. CONNECT TO THE PROPER mongod PROCESS
 //
-db = connect(myName+".local:27000/test");
-//
-// 2. INITIATE THE REPLICA SET
-//
-res = rs.initiate(
-    {
-        "_id" : "dublin",
-        "members" : [
-            { _id:0,host:myName+".local:"+"27000" },
-            { _id:1,host:myName+".local:"+"27001" },
-            { _id:2,host:myName+".local:"+"27002" }
-        ]
+var clusters = ["Dublin", "Cork", "Limerick", "Galway"];
+var index = 0;
+var basePort = 27000;
+
+for (index = 0; index < clusters.length; index++) {
+    var city = clusters[index];
+
+    db = connect(myName + ".local:" + basePort + "/test");
+    res = rs.initiate(
+        {
+            "_id" : city.toLowerCase(),
+            "members" : [
+                { _id:0,host:myName+".local:"+basePort },
+                { _id:1,host:myName+".local:"+(basePort + 1) },
+                { _id:2,host:myName+".local:"+(basePort + 2) }
+            ]
+        }
+    );
+    //
+    // 3. WAIT UNTIL ALL THE NODES OF THE REPLICA SET ARE UP AND RUNNING
+    // //
+    while (res.ok != 1){
+        sleep(10);
     }
-);
-//
-// 3. WAIT UNTIL ALL THE NODES OF THE REPLICA SET ARE UP AND RUNNING
-//
-while (res.ok != 1){
-    sleep(10);
-}
-print("Dublin Replica Set Created!");
-while (((rs.status().members[0].state != 1) && (rs.status().members[0].state != 2)) || ((rs.status().members[1].state != 1) && (rs.status().members[1].state != 2)) || ((rs.status().members[2].state != 1) && (rs.status().members[2].state != 2))) {
-    sleep(10);
-}
-print("Dublin Replica Set Up!");
-//
-// 4. ADD THE SHARD
-//
-db = mongosConn;
-res = sh.addShard("dublin/"+myName+".local:27000");
-while (res.ok != 1){
-    sleep(60);
-    if (res.ok != 1){
-        print("Adding Shard Failed. Trying it again");
-        res = sh.addShard("dublin/"+myName+".local:27000");
+    print(city + " Replica Set Created!");
+    while (((rs.status().members[0].state != 1) && (rs.status().members[0].state != 2)) || ((rs.status().members[1].state != 1) && (rs.status().members[1].state != 2)) || ((rs.status().members[2].state != 1) && (rs.status().members[2].state != 2))) {
+        sleep(10);
     }
-}
-print("Dublin Shard Added!");
-//
-//------------------------------------------------
-// 4. CORK
-//------------------------------------------------
-//
-// 1. CONNECT TO THE PROPER mongod PROCESS
-//
-db = connect(myName+".local:27100/test");
-//
-// 2. INITIATE THE REPLICA SET
-//
-res = rs.initiate(
-    {
-        "_id" : "cork",
-        "members" : [
-            { _id:0,host:myName+".local:"+"27100" },
-            { _id:1,host:myName+".local:"+"27101" },
-            { _id:2,host:myName+".local:"+"27102" }
-        ]
+    print(city + " Replica Set Up!");
+    //
+    // 4. ADD THE SHARD
+    //
+    db = mongosConn;
+    res = sh.addShard(city.toLowerCase() + "/" + myName + ".local:" + basePort);
+    while (res.ok != 1){
+        sleep(60);
+        if (res.ok != 1){
+            print("Adding Shard Failed. Trying it again");
+            res = sh.addShard(city.toLowerCase() + "/" + myName + ".local:" + basePort);
+        }
     }
-);
-//
-// 3. WAIT UNTIL ALL THE NODES OF THE REPLICA SET ARE UP AND RUNNING
-//
-while (res.ok != 1){
-    sleep(10);
+    print(city + " Shard Added!");
+    basePort += 100;
 }
-print("Cork Replica Set Created!");
-while (((rs.status().members[0].state != 1) && (rs.status().members[0].state != 2)) || ((rs.status().members[1].state != 1) && (rs.status().members[1].state != 2)) || ((rs.status().members[2].state != 1) && (rs.status().members[2].state != 2))) {
-    sleep(10);
-}
-print("Cork Replica Set Up!");
-//
-// 4. ADD THE SHARD
-//
-db = mongosConn;
-res = sh.addShard("cork/"+myName+".local:27100");
-while (res.ok != 1){
-    sleep(60);
-    if (res.ok != 1){
-        print("Adding Shard Failed. Trying it again");
-        res = sh.addShard("cork/"+myName+".local:27100");
-    }
-}
-print("Cork Shard Added!");
-//
-//------------------------------------------------
-// 5. LIMERICK
-//------------------------------------------------
-//
-// 1. CONNECT TO THE PROPER mongod PROCESS
-//
-db = connect(myName+".local:27200/test");
-//
-// 2. INITIATE THE REPLICA SET
-//
-res = rs.initiate(
-    {
-        "_id" : "limerick",
-        "members" : [
-            { _id:0,host:myName+".local:"+"27200" },
-            { _id:1,host:myName+".local:"+"27201" },
-            { _id:2,host:myName+".local:"+"27202" }
-        ]
-    }
-);
-//
-// 3. WAIT UNTIL ALL THE NODES OF THE REPLICA SET ARE UP AND RUNNING
-//
-while (res.ok != 1){
-    sleep(10);
-}
-print("Limerick Replica Set Created!");
-while (((rs.status().members[0].state != 1) && (rs.status().members[0].state != 2)) || ((rs.status().members[1].state != 1) && (rs.status().members[1].state != 2)) || ((rs.status().members[2].state != 1) && (rs.status().members[2].state != 2))) {
-    sleep(10);
-}
-print("Limerick Replica Set Up!");
-//
-// 4. ADD THE SHARD
-//
-db = mongosConn;
-res = sh.addShard("limerick/"+myName+".local:27200");
-while (res.ok != 1){
-    sleep(60);
-    if (res.ok != 1){
-        print("Adding Shard Failed. Trying it again");
-        res = sh.addShard("limerick/"+myName+".local:27200");
-    }
-}
-print("Limerick Shard Added!");
-//
-//------------------------------------------------
-// 6. GALWAY
-//------------------------------------------------
-//
-// 1. CONNECT TO THE PROPER mongod PROCESS
-//
-db = connect(myName+".local:27300/test");
-//
-// 2. INITIATE THE REPLICA SET
-//
-res = rs.initiate(
-    {
-        "_id" : "galway",
-        "members" : [
-            { _id:0,host:myName+".local:"+"27300" },
-            { _id:1,host:myName+".local:"+"27301" },
-            { _id:2,host:myName+".local:"+"27302" }
-        ]
-    }
-);
-//
-// 3. WAIT UNTIL ALL THE NODES OF THE REPLICA SET ARE UP AND RUNNING
-//
-while (res.ok != 1){
-    sleep(10);
-}
-print("Galway Replica Set Created!");
-while (((rs.status().members[0].state != 1) && (rs.status().members[0].state != 2)) || ((rs.status().members[1].state != 1) && (rs.status().members[1].state != 2)) || ((rs.status().members[2].state != 1) && (rs.status().members[2].state != 2))) {
-    sleep(10);
-}
-print("Galway Replica Set Up!");
-//
-// 4. ADD THE SHARD
-//
-db = mongosConn;
-res = sh.addShard("galway/"+myName+".local:27300");
-while (res.ok != 1){
-    sleep(60);
-    if (res.ok != 1){
-        print("Adding Shard Failed. Trying it again");
-        res = sh.addShard("galway/"+myName+".local:27300");
-    }
-}
-print("Galway Shard Added!");
-//
-//------------------------------------------------
-// 7. QUIT
-//------------------------------------------------
-//
 quit()
