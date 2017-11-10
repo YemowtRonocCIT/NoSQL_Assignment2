3b233cda6fe5c6d118cf66fa7e0a5184b813902d
Conor Twomey <conor.twomey1@mycit.ie>
2017-11-10 12:56:21 +0000

    Clean up file format

--- 3.insert_collection.bat
+++ 3.insert_collection.bat
@@ -6,8 +6,9 @@ REM #						        #
 REM #---------------------------------------------------#
 REM #	
 SET original_directory="%cd%"
-cd C:\"Program Files"\MongoDB\Server\3.0\bin
 SET mongo_import_exe=mongoimport.exe
+
+cd C:\"Program Files"\MongoDB\Server\3.0\bin
 %mongo_import_exe% --db test --collection restaurants --drop --file %original_directory%\3.restaurants_dataset.json
 cd %original_directory%
 REM #
